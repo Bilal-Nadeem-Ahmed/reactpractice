6be6c26889d6d98c90465f0b34df1e82a19010ad
Bilal Ahmed <bilal91_@hotmail.com>
2021-04-08 15:45:53 +0100

    import and use Divider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Buttons from './components/Button';
+import Deviders from './components/Deviders';
 import Icons from './components/Icons';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <h1>antd practice</h1>
       <Buttons/>
       <Icons/>
+      <Deviders/>
     </div>
   )
 }
