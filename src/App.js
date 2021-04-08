f4308524c27517574c335eed46848b3843d0b771
Bilal Ahmed <bilal91_@hotmail.com>
2021-04-09 00:56:00 +0100

    adjusted navigation

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import Buttons from './components/Button';
 import Deviders from './components/Deviders';
 import Icons from './components/Icons';
+import Layouts from './components/layout';
 
 function App() {
   return (
     <div className="App">
+      <Layouts/>
       <h1>antd practice</h1>
       <Buttons/>
       <Icons/>
--- src/components/Deviders/index.js
+++ src/components/Deviders/index.js
@@ -2,6 +2,7 @@ import { Divider } from 'antd';
 const Deviders = () => {
     return ( 
         <>
+        <h1>Divider section</h1>
          <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
       probare, quae sunt a te dicta? Refert tamen, quo modo.
