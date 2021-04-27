97104172db0fd3120521d34cdd314bb76733f510
Bilal Ahmed <bilal91_@hotmail.com>
2021-04-27 01:26:37 +0100

    added routing for id requests

--- f3/index.js
+++ f3/index.js
@@ -1,5 +1,4 @@
-// const http =require('http')
-const { request, response } = require('express')
+// const { request, response } = require('express')
 const express = require('express')
 const app = express()
 
@@ -25,21 +24,25 @@ let notes = [
   ]
 
 
-// const app= http.createServer( (request,response) =>{
-//     response.writeHead(200,{'Content-type':'application/json'})
-//     response.end(JSON.stringify(notes))
-// })
 
-// const PORT =3001
-// app.listen(PORT)
-// console.log(`server running on port ${PORT}`)
 
 app.get('/',(request,response)=>{
-  response.send('<h1>Hello worl</h1>')
+  response.send('<h1>Hello world</h1>')
 })
 app.get('/api/notes', (request,response)=>{
   response.json(notes)
 })
+app.get('/api/notes/:id',(request,response)=>{
+  const id= Number(request.params.id)
+  const note=notes.find(note=> note.id===id)
+  if(note){
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+  
+})
+
 const PORT =3001
 app.listen(PORT,()=>{
   console.log('server running on port ' + PORT)
