a3906225cb00fdae03464fbd58b5d1ba4962d885
Bilal Ahmed <bilal91_@hotmail.com>
2021-05-25 19:51:59 +0100

    put validation in the middleware

--- f3/index.js
+++ f3/index.js
@@ -75,23 +75,23 @@ app.put('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/notes',(request,response)=>{
+app.post('/api/notes',(request,response,next)=>{
 
   const body = request.body
 
-  if(body.content===undefined) {
-    return response.status(400).json({error:'content missing'})
-  }
-
+ 
   const note = new Note({
     content: body.content,
     
     important: body.important || false,
     date: new Date()
   })
- note.save().then(savedNote=>{
-   response.json(savedNote)
+ note.save()
+ .then(savedNote=> savedNote.toJSON())
+ .then(savedAndFormattedNote=>{
+   response.json(savedAndFormattedNote)
  })
+ .catch(error=>next(error))
   
   // response.json(note)
 })
@@ -121,7 +121,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError'){
+    return response.status(400).json({error: error.message})
+  }
 
   next(error)
 }
