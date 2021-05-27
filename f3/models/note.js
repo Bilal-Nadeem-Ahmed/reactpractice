46ddf21b95e5df318dcfac5d3bbbad275a6fbb7d
Bilal Ahmed <bilal91_@hotmail.com>
2021-05-27 12:46:19 +0100

    changed file structure

--- f3/models/note.js
+++ f3/models/note.js
@@ -1,17 +1,5 @@
 const mongoose = require('mongoose')
 
-// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
-// const url =
-// `mongodb+srv://user1:MangoYogurt1@cluster0.yaoc2.mongodb.net/notesapp?retryWrites=true&w=majority`
-
-const url = process.env.MONGODB_URI
-console.log('connected  to', url)
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-.then((result)=>{console.log('connected to mongodb')})
-.catch((error)=>{
-    console.log('error connecting to mongodb:',error.message)
-})
 const noteSchema = new mongoose.Schema({
   content: {
     type : String,
@@ -26,7 +14,7 @@ const noteSchema = new mongoose.Schema({
 })
 
 noteSchema.set('toJSON',{
-  transform:(document,returnedObject)=>{
+  transform:(document,returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
---
+++ f3/utils/logger.js
@@ -0,0 +1,12 @@
+// for printing normal log messages
+const info = (...params) => {
+  console.log(...params)
+}
+//for printing error messages
+const error =(...params) => {
+  console.error(...params)
+}
+
+module.exports={
+  info,error
+}
