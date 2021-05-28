c59b696ef36f0f6d5e0a9943f5d23d2b9870f452
Bilal Ahmed <bilal91_@hotmail.com>
2021-05-28 12:59:06 +0100

    wrote my first tests

---
+++ f3/tests/palindrome.test.js
@@ -0,0 +1,18 @@
+const palindrome = require('../utils/for_testing').palindrome
+
+test('palindrome of a ', () => {
+  const result = palindrome('a')
+
+  expect(result).toBe('a')
+})
+
+test('palindrome of react',() => {
+  const result = palindrome('react')
+
+  expect (result).toBe('tcaer')
+})
+
+test('Palindrome of racecar',() => {
+  const result=palindrome('racecar')
+  expect(result).toBe('racecar')
+})
