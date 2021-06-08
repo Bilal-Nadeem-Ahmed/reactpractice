ec7b9841a93f1240a499b44ea938510713f5182a
Bilal Ahmed <bilal91_@hotmail.com>
2021-06-08 15:58:02 +0100

    test database works with tesing

--- f3/utils/config.js
+++ f3/utils/config.js
@@ -5,4 +5,6 @@ const MONGODB_URI = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
 
+// const MONGODB_URI =  process.env.TEST_MONGODB_URI
+
 module.exports={ PORT,MONGODB_URI }
