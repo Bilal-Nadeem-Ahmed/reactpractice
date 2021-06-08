acc0385673230966105679ee8d15f7da98ef6e41
Bilal Ahmed <bilal91_@hotmail.com>
2021-06-08 16:16:52 +0100

    changed logger to not log to console during testing

--- f3/tests/note_api.test.js
+++ f3/tests/note_api.test.js
@@ -11,7 +11,20 @@ test('notes are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
+test('there are to notes', async () => {
+  const response = await api.get('/api/notes')
+  // execution gets here only after the HTTP request is complete
+  // the result of HTTP request is saved in variable response
+  expect(response.body).toHaveLength(2)
+})
+
+test('the first notes content is gibberish', async () => {
+  const response = await api.get('/api/notes')
+  expect(response.body[0].content).toBe('jyhtdrffjhgdf')
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
+
 
--- f3/utils/logger.js
+++ f3/utils/logger.js
@@ -1,10 +1,14 @@
 // for printing normal log messages
 const info = (...params) => {
-  console.log(...params)
+  if (process.env.NODE_ENV !== 'test') {
+    console.log(...params)
+  }
 }
 //for printing error messages
 const error =(...params) => {
-  console.error(...params)
+  if (process.env.NODE_ENV !== 'test') {
+    console.error(...params)
+  }
 }
 
 module.exports={
