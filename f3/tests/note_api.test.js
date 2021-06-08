1d7202a585a9d32872ba403f0cfb4b2e8060473a
Bilal Ahmed <bilal91_@hotmail.com>
2021-06-08 16:28:48 +0100

    made changes to tests

--- f3/tests/note_api.test.js
+++ f3/tests/note_api.test.js
@@ -4,6 +4,31 @@ const app = require('../app')
 
 const api = supertest(app)
 
+const Note = require('../models/note')
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    date: new Date(),
+    important: false,
+  },
+  {
+    content: 'Browser can execute only Javascript',
+    date: new Date(),
+    important: true,
+  },
+]
+//Runs a function before each of the tests in this file runs. If the function returns a promise or is a generator, Jest waits for that promise to resolve before running the test.
+beforeEach(async () => {
+  await Note.deleteMany({})
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
+})
+
+
+
+
 test('notes are returned as json', async () => {
   await api
     .get('/api/notes')
@@ -11,16 +36,18 @@ test('notes are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
-test('there are to notes', async () => {
+test('all notes are returned', async () => {
   const response = await api.get('/api/notes')
   // execution gets here only after the HTTP request is complete
   // the result of HTTP request is saved in variable response
-  expect(response.body).toHaveLength(2)
+  expect(response.body).toHaveLength(initialNotes.length)
 })
 
-test('the first notes content is gibberish', async () => {
+test('the first notes content is HTML is easy', async () => {
   const response = await api.get('/api/notes')
-  expect(response.body[0].content).toBe('jyhtdrffjhgdf')
+
+  const contents = response.body.map(r => r.content)
+  expect(contents).toContain('Browser can execute only Javascript')
 })
 
 afterAll(() => {
