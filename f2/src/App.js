9ff36c24e48e6ecea89c04747680f92bbe48d103
Bilal Ahmed <bilal91_@hotmail.com>
2021-07-19 12:04:09 +0100

    started to implement sign in on front end

--- f2/src/App.js
+++ f2/src/App.js
@@ -7,6 +7,8 @@ import noteService from './services/notes'
 function App() {
   const [notes,setNotes]=useState([]);
   console.log(noteService)
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('')
 
   useEffect(()=>{
     console.log('effect')
@@ -18,12 +20,37 @@ function App() {
 
   },[])
   console.log('render', notes.length, 'notes')
+  const handleLogin = (event)=>{
+    event.preventDefault()
+    console.log('logging in with', username,password)
+  }
 
  
   return (
     <div className="App">
 
       <Application notes={notes} setNotes={setNotes}/>
+      <form>
+        <div>
+          username 
+          <input
+          type='text'
+          value={username}
+          name = 'Username'
+          onChange={({target})=>setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+          type='password'
+          value={password}
+          name = 'Password'
+          onChange={({target})=>setPassword(target.value)}
+          />
+        </div>
+        <button type='submit'>login</button>
+      </form>
     </div>
   )
 }
